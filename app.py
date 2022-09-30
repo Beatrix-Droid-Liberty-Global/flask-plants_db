43fd9c8b2c4c0161214919cd0fe526c70869108e
Beatrice <beatrice.federici123@gmail.com>
2022-09-30 11:26:44 +0000

    implemented db

--- app.py
+++ app.py
@@ -1,119 +1,94 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, url_for, flash, request, redirect
+from flask_sqlalchemy  import SQLAlchemy
 from config import *
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import current_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, login_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-app.config['SESSION_TYPE'] = 'filesystem'
+app.config["SECRET_KEY"]="49132rh9ehfr2eh234"
+bcrypt = Bcrypt(app)
+db= SQLAlchemy(app)
+login_manager=LoginManager()
+login_manager.init_app(app)
+login_manager.login_view ="login"
 
-app.config['SECRET_KEY'] = 'my-secret-key'
-app.config["=SQLALCHEMY_DATABASE_URI"]="sqlite:///Users.sqlite3"
 
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
-
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     _id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column("name", db.String(100), nullable=False, unique=True)
-    
-    #password stuff
-    password_hash = db.Column("password", db.String(100))
+    username = db.Column("name", db.String(100), nullable=False, unique=True)
+    password = db.Column("password", db.String(100), nullable=False)
 
-    @property
-    def password(self):
-        raise AttributeError("password is not a readable attribute")
 
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=100)], render_kw={"placeholder":"Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=100)], render_kw={"placeholder": "password"})
+    submit = SubmitField("Register")
 
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    def validate_username(self, username):
+        existing_user_username = Users.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError("That username already exists. Please pick another one.")
 
-    confirm_password = db.Column("confirm_password", db.String(100))
 
-    def __init__(self, name, password_hash, confirm_password=None):
-        self.name = name
-        self.password_hash = password_hash
-        self.confirm_password = confirm_password
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=100)], render_kw={"placeholder":"Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=100)], render_kw={"placeholder": "password"})
+    submit = SubmitField("Login")
+
 
 db.create_all()
 
-@app.route('/log', methods=["POST", "GET"])
-def log():
-    if request.method == "POST":
-        
-        username = request.form['username']
-        password = request.form['password']
-        found_user = Users.query.filter_by(name=username).first()
-    
-        if found_user:
-            #hash the password!
-            passed =check_password_hash(found_user.password_hash, password)
-            if passed:
-                return  redirect(url_for('view_plants'))
-            else:
-                flash("Username or Password must be incorrect, please check details again")  
-                return render_template("index.html")
-    return render_template("index.html")
+@app.route("/log", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        #check if user is in db
+        user = Users.query.filter_by(username = form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password,form.password.data):
+                login_user(user)
+                return redirect(url_for("view_plants"))
+        flash("Username or password entered incorrectly. Please try entering them again.")
+
+    return render_template("index.html", form=form)
 
 
-@app.route('/new_user', methods=["POST", "GET"])
+
+@app.route("/new_user", methods=["GET", "POST"])
 def register_user():
+    form = RegisterForm()
     if request.method == "POST":
-        confirm_username = request.form['new_username']
-        new_password = request.form['new_password']
-        confirm_password =request.form["confirm_password"]
-        #check that the new passwords the new user has created exists
-        if new_password != confirm_password:
-            flash("password input fields don't match, please check again")
-            return redirect(url_for('register_user'))
-
-        #check that the username isn't already taken
-        if Users.query.filter_by(name=confirm_username).first():
-            flash("Username is already taken. Please pick another one to use.")
-            return redirect(url_for('register_user'))
-
-        
-        #logic to register new user
-        hashed_password = generate_password_hash(new_password, "sha256")
-
-        usr = Users(name=confirm_username, password_hash=hashed_password)
-        db.session.add(usr)
-        db.session.commit()
-        return  redirect(url_for('log'))
-
-    return render_template("new_user.html")
-
-
-@app.route("/delete_user")
-
-def delete_user(id):
-    if id ==current_user.id:
-        user_to_delete = Users.query.get_or_404(id)
-        try:
-            db.session.delete(user_to_delete)
+        if form.validate_on_submit():
+            hashed_password = bcrypt.generate_password_hash(form.password.data)
+            new_user = Users(username=form.username.data, password= hashed_password)
+            db.session.add(new_user)
             db.session.commit()
-            flash("User deleted successfully")
-            return redirect(url_for('log'))
-        except:
-            flash("there was a problem deleting user")
-            return redirect(url_for('log'))
+            return redirect(url_for("login"))
+        #insert something here
+        flash("Username already exists, please pick another one")
+    return render_template("new_user.html", form=form)
 
 
-@app.route('/view_plants', methods=["POST"])
+@app.route("/view_plants", methods =["POST"])
+@login_required
 def view_plants():
-
     return render_template("your_plants.html")
 
-@app.route("/view")
-def view():
-    return render_template("view.html", values=Users.query.all())
+@app.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 if __name__ == "__app__":
-   
     app.run(debug=True)
